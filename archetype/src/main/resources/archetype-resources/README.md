91e82ecefc88d5be325c013342bc0b0666b073a7
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2020-06-22 10:41:49 +0200

    A better archetype is always better

--- archetype/src/main/resources/archetype-resources/README.md
+++ archetype/src/main/resources/archetype-resources/README.md
@@ -4,6 +4,7 @@ As an example, you could use [readme-md-generator](https://github.com/kefranabg/
 by running the command `npx readme-md-generator`.
 
 However, to build your application, just run `mvn install` and you'll be good to go.
-If you want finer control/faster architect experience, you can also use [fizzed-watcher](https://github.com/fizzed/maven-plugins#watcher-fizzed-watcher-maven-plugin).
-Typically, you can have documents updated live by running `mvn fizzed-watcher:run@watch-docs` 
-and have slides updated live by running `mvn fizzed-watcher:run@watch-slides`.
+If you want finer control/faster architect experience, you can also use the provided livereload profile,
+as stated in main documentation.
+In other words, run `mvn -Pgenerate-docs -Plivereload` and go on [http://localhost:35729/docs/html/](http://localhost:35729/docs/html/). 
+If your browser has a livereload extension, it'll automagically reload the documentation each time it is built. 
--- archetype/src/main/resources/archetype-resources/src/main/java/Architecture.java
+++ archetype/src/main/resources/archetype-resources/src/main/java/Architecture.java
@@ -7,9 +7,11 @@ import javax.enterprise.context.ApplicationScoped;
 import org.ndx.agile.architecture.base.ArchitectureModelProvider;
 
 import com.structurizr.Workspace;
+import com.structurizr.model.Container;
 import com.structurizr.model.Model;
 import com.structurizr.model.Person;
 import com.structurizr.model.SoftwareSystem;
+import com.structurizr.view.ContainerView;
 import com.structurizr.view.SystemContextView;
 import com.structurizr.view.ViewSet;
 
@@ -30,11 +32,17 @@ public class Architecture implements ArchitectureModelProvider {
 		SoftwareSystem softwareSystem = model.addSoftwareSystem("Software System", "My software system.");
 		user.uses(softwareSystem, "Uses");
 
+		Container aContainer = softwareSystem.addContainer("TODO", "An example container", "What technology do you use?");
+		user.uses(aContainer, "Do something");
+		/////////////////////////////////////////////////////////////////////////////////////////
 		ViewSet views = workspace.getViews();
 		SystemContextView contextView = views.createSystemContextView(softwareSystem, "SystemContext",
 				"An example of a System Context diagram.");
 		contextView.addAllSoftwareSystems();
 		contextView.addAllPeople();
+
+		ContainerView softwareSystemContainers = views.createContainerView(softwareSystem, "software.system.containers", "Software system containers");
+		softwareSystemContainers.addAllContainersAndInfluencers();
 
 //		Styles styles = views.getConfiguration().getStyles();
 //		styles.addElementStyle(Tags.SOFTWARE_SYSTEM).background("#1168bd").color("#ffffff");
