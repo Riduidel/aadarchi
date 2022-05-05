81c836536004675e7643f4ddd4032602646c1911
Bullfrog <syczjason@gmail.com>
2022-05-05 10:17:26 +0200

    logger build ok

--- aadarchi-maven-plugin/src/main/java/org/ndx/agile/architecture/documentation/system/maven/plugin/GenerateDiagramsMojo.java
+++ aadarchi-maven-plugin/src/main/java/org/ndx/agile/architecture/documentation/system/maven/plugin/GenerateDiagramsMojo.java
@@ -3,20 +3,15 @@ package org.ndx.agile.architecture.documentation.system.maven.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
-import org.ndx.agile.architecture.base.ArchitectureDocumentationBuilder;
 
-import javax.inject.Inject;
 import java.util.Arrays;
 import java.util.logging.Logger;
 
-@Mojo(name = "generate-model", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
+@org.apache.maven.plugins.annotations.Mojo(name = "generate-model", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
 public class GenerateDiagramsMojo extends AbstractMojo {
-    private static final Logger logger = Logger.getLogger(Mojo.class.getName());
+    private static final Logger logger = Logger.getLogger(GenerateDiagramsMojo.class.getName());
 
     @Parameter(required = true)
     public String name;
@@ -27,4 +22,5 @@ public class GenerateDiagramsMojo extends AbstractMojo {
         root.addHandler(new MavenLoggingRedirectorHandler(getLog()));
         logger.info("I'm writing a message");
     }
+
 }
