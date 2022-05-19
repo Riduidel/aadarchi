f563d22ca84ef2e34e7e792eb6733cf91df7d7c3
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2022-05-19 15:51:41 +0200

    Fixing the last changes preventing "normal" debug of the plugin

--- cdi-config-extension/src/main/java/org/ndx/agile/architecture/cdi/FileConfigPropertyProducer.java
+++ cdi-config-extension/src/main/java/org/ndx/agile/architecture/cdi/FileConfigPropertyProducer.java
@@ -13,12 +13,22 @@ import org.apache.deltaspike.core.spi.config.BaseConfigPropertyProducer;
 @ApplicationScoped
 public class FileConfigPropertyProducer extends BaseConfigPropertyProducer {
 
+	/**
+	 * When an injection point is declared with the @ConfigProperty annotation and has the File type,
+	 * this method creates a file using the configured value as name
+	 * @param injectionPoint
+	 * @return
+	 */
 	@Produces
 	@Dependent
     @ConfigProperty(name = "ignored") // we actually don't need the name
 	public File produceLocationId(InjectionPoint injectionPoint) {
 		String configuredValue = getStringPropertyValue(injectionPoint);
 
-		return new File(configuredValue.trim());
+		if(configuredValue==null) {
+			throw new NullPointerException(String.format("Unable to inject a file which @ConfigProperty value is null at injection point %s", injectionPoint));
+		} else {
+			return new File(configuredValue.trim());
+		}
 	}
 }
