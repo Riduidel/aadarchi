b7d8243c2084201a4badf9e130d75412beb07d8a
Bullfrog <syczjason@gmail.com>
2022-05-24 15:25:54 +0200

    create GenerateHtmlDoc

--- aadarchi-maven-plugin/src/main/java/org/ndx/agile/architecture/documentation/system/maven/plugin/GenerateHtmlDocs.java
+++ aadarchi-maven-plugin/src/main/java/org/ndx/agile/architecture/documentation/system/maven/plugin/GenerateHtmlDocs.java
@@ -1,16 +1,5 @@
 package org.ndx.agile.architecture.documentation.system.maven.plugin;
 
-import static org.twdata.maven.mojoexecutor.MojoExecutor.artifactId;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.configuration;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.element;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.executeMojo;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.executionEnvironment;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.goal;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.groupId;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.name;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.plugin;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.version;
-
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.BuildPluginManager;
@@ -20,7 +9,9 @@ import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;.*
+import org.apache.maven.project.MavenProject;
+
+import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
 
 @Mojo(name = "generate-html-docs", defaultPhase = LifecyclePhase.PREPARE_PACKAGE)
 public class GenerateHtmlDocs extends AbstractMojo {
@@ -34,20 +25,38 @@ public class GenerateHtmlDocs extends AbstractMojo {
 	private BuildPluginManager pluginManager;
 
 	@Parameter(name = "asciidoctor-maven-plugin-version", defaultValue = "2.1.0")
-	private String asciidoctorMavenPluinVersion;
-	
+	private String asciidoctorMavenPluginVersion;
+
+	@Parameter(name = "asciidoctorj-pdf-version", defaultValue = "1.5.4")
+	private String asciidoctorjPdfVersion;
+
+	@Parameter(name = "jruby-version", defaultValue = "9.2.9.0")
+	private String jrubyVersion;
+
+	@Parameter(name = "asciidoctorj-version", defaultValue = "2.4.3")
+	private String asciidoctorjVersion;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		executeMojo(
 			    plugin(
 			        groupId("org.asciidoctor"),
 			        artifactId("asciidoctor-maven-plugin"),
-			        version(asciidoctorMavenPluinVersion),
-			        dependencies(...)
+			        version(asciidoctorMavenPluginVersion),
+			        dependencies(
+							dependency("org.asciidoctor", "asciidoctorj-pdf", asciidoctorjPdfVersion),
+							dependency("org.jruby", "jruby-complete", jrubyVersion),
+							dependency("org.asciidoctor", "asciidoctorj", asciidoctorjVersion)
+					)
 			    ),
-			    goal("copy-dependencies"),
+			    goal("process-asciidoc"),
 			    configuration(
-			        element(name("outputDirectory"), "${project.build.directory}/foo")
+						element(name("gemPath"), "${project.build.directory}/gems"),
+						element(name("attrributes"),
+							element(name("allow-uri-read")),
+							element(name("kroki-server-url"), "TODO create variable")
+								),
+						element(name("outputDirectory"), "${project.build.directory}/foo")
 			    ),
 			    executionEnvironment(
 			        mavenProject,
