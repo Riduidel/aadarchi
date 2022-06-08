106a5af7095e8b65750e75aeec0c94538c377a6b
Bullfrog <syczjason@gmail.com>
2022-06-08 17:08:43 +0200

    pom info in LiveReload.java

--- aadarchi-maven-plugin/src/main/java/org/ndx/agile/architecture/documentation/system/maven/plugin/LiveReload.java
+++ aadarchi-maven-plugin/src/main/java/org/ndx/agile/architecture/documentation/system/maven/plugin/LiveReload.java
@@ -19,23 +19,47 @@ public class LiveReload extends AbstractMojo {
 	@Parameter(name = "kroki-server-url", defaultValue = "${kroki.server.url}")
 	private String krokiServerUrl;
 
+	@Component
+	private MavenProject mavenProject;
+
+	@Component
+	private MavenSession mavenSession;
+
+	@Component
+	private BuildPluginManager pluginManager;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		executeMojo(
 				plugin(
-						groupId("org.asciidoctor"),
-						artifactId("asciidoctor-maven-plugin"),
-						version(asciidoctorMavenPluginVersion),
+						groupId("org.codehaus.mojo"),
+						artifactId("exec-maven-plugin"),
+						version(""),
 						dependencies(
-								dependency("org.asciidoctor", "asciidoctorj", asciidoctorjVersion)
+								dependency("net.alchim31", "livereload-jvm", "0.2.0")
 						)
 				),
-				goal("process-asciidoc"),
+				goal("exec"),
 				configuration(
-			    		// TODO conditionalize that invocation : add all gems dependencies here
-			    		element(name("requires"),
-								element(name("require"), "asciidoctor-kroki")),
-						element(name("gemPath"), "${project.build.directory}/gems")
+						element(name("executable"), "java"),
+						element(name("arguments"),
+								element(name("argument"), "-classpath"),
+								element(name("classpath"),
+										element(name("dependency"), "net.alchim31:livereload-jvm"),
+										element(name("dependency"), "org.eclipse.jetty:jetty-util"),
+										element(name("dependency"), "org.eclipse.jetty:jetty-io"),
+										element(name("dependency"), "org.eclipse.jetty.orbit:javax.servlet:jar"),
+										element(name("dependency"), "org.eclipse.jetty:jetty-continuation"),
+										element(name("dependency"), "org.eclipse.jetty:jetty-server"),
+										element(name("dependency"), "org.eclipse.jetty.orbit:javax.servlet"),
+										element(name("dependency"), "org.eclipse.jetty:jetty-http"),
+										element(name("dependency"), "com.googlecode.json-simple:json-simple"),
+										element(name("dependency"), "org.eclipse.jetty:jetty-websocket")),
+								element(name("argument"), "net_alchim31_livereload.Main"),
+								element(name("argument"), "-"),
+								element(name("argument"), "${asciidoc.target.base.directory}")),
+						element(name("async"), "true"),
+						element(name("asyncDestroyOnShutdown"), "true")
 				),
 				executionEnvironment(
 						mavenProject,
