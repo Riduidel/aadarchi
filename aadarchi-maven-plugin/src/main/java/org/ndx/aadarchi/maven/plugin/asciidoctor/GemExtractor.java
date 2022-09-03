9b73bcebb84959ebe945db56b6a7825eb99fd5dc
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2022-09-03 21:29:27 +0200

    And ... it works ! Suffice to say that for a gem to work, it has to have both the gem content and the specification

--- aadarchi-maven-plugin/src/main/java/org/ndx/aadarchi/maven/plugin/asciidoctor/GemExtractor.java
+++ aadarchi-maven-plugin/src/main/java/org/ndx/aadarchi/maven/plugin/asciidoctor/GemExtractor.java
@@ -17,9 +17,73 @@ import org.twdata.maven.mojoexecutor.MojoExecutor.Element;
 public class GemExtractor {
 
 	private Log log;
-	private FileSystemManager fsManager;
-	private Map<String, FileObject> bundledGems;
+	/**
+	 * Commons VFS file manager used as long as this object exists
+	 */
+	private static FileSystemManager fsManager;
+	/**
+	 * Map linking a gem name to the elements required to unpack it
+	 * (link name to versionned name that is used to unpack elements)
+	 */
+	private Map<String, GemInfo> bundledGems;
 	private String gemsPath;
+	
+	public enum GemElement {
+		gems {
+			@Override
+			FileObject pathOf(GemInfo gemInfo, GemContainer container) throws FileSystemException {
+				return container.path(this).resolveFile(gemInfo.versionnedName);
+			}
+		},
+		specifications {
+
+			@Override
+			FileObject pathOf(GemInfo gemInfo, GemContainer container) throws FileSystemException {
+				return container.path(this).resolveFile(gemInfo.versionnedName+".gemspec");
+			}
+			
+		};
+
+		abstract FileObject pathOf(GemInfo gemInfo, GemContainer container) throws FileSystemException;
+	}
+	public abstract class GemContainer {
+
+		FileObject path(GemElement gems) throws FileSystemException {
+			return path().resolveFile(gems.name());
+		}
+
+		abstract FileObject path() throws FileSystemException;
+	}
+	
+	public GemContainer SOURCE = new GemContainer() {
+
+		@Override
+		FileObject path() throws FileSystemException {
+			return getThisJarAsVFS().resolveFile("META-INF").resolveFile("gems");
+		}
+		
+	};
+	public GemContainer TARGET = new GemContainer() {
+
+		@Override
+		FileObject path() throws FileSystemException {
+			return getGemsFolder();
+		}
+		
+	};
+	public class GemInfo {
+		public final String versionnedName;
+
+		@Override
+		public String toString() {
+			return "GemInfo [versionnedName=" + versionnedName + "]";
+		}
+
+		public GemInfo(String versionnedName) {
+			super();
+			this.versionnedName = versionnedName;
+		}
+	}
 
 	public GemExtractor(String gemsPath, Log log) {
 		this.gemsPath = gemsPath;
@@ -38,20 +102,25 @@ public class GemExtractor {
 			for (Xpp3Dom dependency : requires.toDom().getChildren("require")) {
 				String gemName = dependency.getValue();
 				if (getBundledGems().containsKey(gemName)) {
-					copyContainedGem(gemName, getBundledGems().get(gemName));
+					copyContainedGem(getBundledGems().get(gemName));
 				}
 			}
 		return requires;
 	}
 
 
-	private void copyContainedGem(String value, FileObject sourceGem) {
+	private void copyContainedGem(GemInfo gemInfo) {
 		try {
-			FileObject gems = getGemsFolder();
-			FileObject target = gems.resolveFile(sourceGem.getName().getBaseName());
-			target.copyFrom(sourceGem, new AllFileSelector());
+			for(GemElement element : GemElement.values()) {
+				FileObject source = element.pathOf(gemInfo, SOURCE);
+				FileObject target = element.pathOf(gemInfo, TARGET);
+				if(!target.exists()) {
+					log.info(String.format("Copying bundled gem %s into %s", gemInfo.versionnedName, element));
+					target.copyFrom(source, new AllFileSelector());
+				}
+			}
 		} catch (FileSystemException e) {
-			throw new RuntimeException(String.format("Unable to copy gem %s due to", value, e));
+			throw new RuntimeException(String.format("Unable to copy gem %s due to", gemInfo, e));
 		}
 	}
 
@@ -60,26 +129,22 @@ public class GemExtractor {
 		if(!destination.exists()) {
 			destination.createFolder();
 		}
-		FileObject gems = destination.resolveFile("gems");
-		if(!gems.exists()) {
-			gems.createFolder();
-		}
-		return gems;
+		return destination;
 	}
 
 	/**
 	 * Bundled gems should be sub-elements of META-INF/gems
 	 * @return
 	 */
-	private Map<String, FileObject> loadBundledGems() {
+	private Map<String, GemInfo> loadBundledGems() {
 		try {
-			Map<String, FileObject> returned = new TreeMap<>();
-			FileObject gemsFolder = getThisJarAsVFS().getChild("META-INF").getChild("gems").getChild("gems");
+			Map<String, GemInfo> returned = new TreeMap<>();
+			FileObject gemsFolder = SOURCE.path(GemElement.gems);
 			for(FileObject gem : gemsFolder.getChildren()) {
 				if(gem.isFolder()) {
 					String gemVersionnedName = gem.getName().getBaseName();
 					String gemName = gemVersionnedName.substring(0, gemVersionnedName.lastIndexOf('-'));
-					returned.put(gemName, gem);
+					returned.put(gemName, new GemInfo(gemVersionnedName));
 				}
 			}
 			return returned;
@@ -88,10 +153,10 @@ public class GemExtractor {
 		}
 	}
 
-	private FileObject getThisJarAsVFS() {
+	private static FileObject getThisJarAsVFS() {
 		URI currentJarUri;
 		try {
-			currentJarUri = getClass()
+			currentJarUri = GemExtractor.class
 			    .getProtectionDomain()
 			    .getCodeSource()
 			    .getLocation()
@@ -108,7 +173,7 @@ public class GemExtractor {
 		}
 	}
 
-	private FileSystemManager getFileSystemManager() {
+	private static FileSystemManager getFileSystemManager() {
 		if (fsManager == null) {
 			try {
 				fsManager = VFS.getManager();
@@ -119,7 +184,7 @@ public class GemExtractor {
 		return fsManager;
 	}
 	
-	protected Map<String, FileObject> getBundledGems() {
+	protected Map<String, GemInfo> getBundledGems() {
 		if (bundledGems == null) {
 			bundledGems = loadBundledGems();
 		}
