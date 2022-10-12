2a8b694a72421305a4be0c5a96fb1f1aa1089c4b
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2022-10-12 09:54:31 +0200

    Fixes #264 for GitHub projects (#266)

--- github-scm-handler/src/main/java/org/ndx/aadarchi/github/GithubSCMHandler.java
+++ github-scm-handler/src/main/java/org/ndx/aadarchi/github/GithubSCMHandler.java
@@ -1,5 +1,6 @@
 package org.ndx.aadarchi.github;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,8 +12,12 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.jboss.weld.exceptions.UnsupportedOperationException;
 import org.kohsuke.github.GHContent;
 import org.kohsuke.github.GHRepository;
@@ -30,6 +35,7 @@ public class GithubSCMHandler implements SCMHandler {
 	@Inject Logger logger;
 	@Inject GitHub github;
 	@Inject FileContentCache fileCache;
+	@Inject Instance<GitOperator> cloner;
 	@Override
 	public boolean canHandle(String project) {
 		return Constants.isGitHubProject(project);
@@ -95,4 +101,18 @@ public class GithubSCMHandler implements SCMHandler {
 		throw new UnsupportedOperationException(String.format("Can't read the file %s with GitHubSCMHandler", url));
 	}
 
+	@Override
+	public void checkout(String projectUrl, File checkoutLocation) throws IOException {
+		if(Constants.isGitHubProject(projectUrl)) {
+			String project = Constants.getGitHubProjectName(projectUrl);
+			GHRepository repository = github.getRepository(project);
+			String gitTransportUrl = repository.getHttpTransportUrl();
+			try {
+				cloner.get().clone(gitTransportUrl, checkoutLocation.getCanonicalFile());
+			} catch (GitAPIException e) {
+				throw new IOException(String.format("Unable to clone %s to %s", projectUrl, checkoutLocation.getAbsolutePath()), e);
+			}
+		}
+		
+	}
 }
