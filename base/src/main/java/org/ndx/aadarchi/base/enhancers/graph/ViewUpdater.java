c9297ac28eccfbb0ec50fbcd5c319a15a34912ea
Sese-collab <serpil.coskun@zenika.com>
2022-11-14 10:32:30 +0100

    replace equals by containsKey method in ViewUpdater

--- base/src/main/java/org/ndx/aadarchi/base/enhancers/graph/ViewUpdater.java
+++ base/src/main/java/org/ndx/aadarchi/base/enhancers/graph/ViewUpdater.java
@@ -36,14 +36,14 @@ public class ViewUpdater extends ViewEnhancerAdapter {
 	
 	@Override
 	protected void endVisit(ComponentView c, OutputBuilder builder) {
-		if(c.getProperties().equals(NAME)) {
+		if(c.getProperties().containsKey(NAME)) {
 			c.addAllComponents();
 		}
 		super.endVisit(c, builder);
 	}
 	@Override
 	protected void endVisit(ContainerView c, OutputBuilder builder) {
-		if (c.getProperties().equals(NAME)) {
+		if (c.getProperties().containsKey(NAME)) {
 			c.addAllContainersAndInfluencers();
 		}
 		super.endVisit(c, builder);
