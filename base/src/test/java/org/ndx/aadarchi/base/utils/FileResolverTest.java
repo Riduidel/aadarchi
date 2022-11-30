9bc78e1141fd6b18c9ecb24e6d5d1a7aca7cf9f6
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2022-11-30 11:10:22 +0100

    Features/291 cant resolve full path (#294)

---
+++ base/src/test/java/org/ndx/aadarchi/base/utils/FileResolverTest.java
@@ -0,0 +1,38 @@
+package org.ndx.aadarchi.base.utils;
+
+import java.io.File;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+import org.assertj.core.api.Assertions;
+
+class FileResolverTest {
+
+	@Test
+	void can_resolve_subfolder() {
+		FileResolver resolver = new FileResolver();
+		// Given
+		// This file resolves as project base execution dir 
+		String userDir = System.getProperty("user.dir");
+		File userFile = new File(userDir);
+		resolver.basedir = new File(userDir);
+		// When
+		Path resolved = resolver.fileAsUrltoPath("src/main/java");
+		// Then
+		Assertions.assertThat(resolved).isEqualTo(new File(userFile, "src/main/java").toPath());
+	}
+	@Test
+	void bug_291_absolute_path_cant_be_resolved() {
+		FileResolver resolver = new FileResolver();
+		// Given
+		// This file resolves as project base execution dir 
+		String userDir = System.getProperty("user.dir");
+		File userFile = new File(userDir);
+		resolver.basedir = new File(userDir);
+		// When
+		Path resolved = resolver.fileAsUrltoPath(userDir);
+		// Then
+		Assertions.assertThat(resolved).isEqualTo(userFile.toPath());
+	}
+
+}
