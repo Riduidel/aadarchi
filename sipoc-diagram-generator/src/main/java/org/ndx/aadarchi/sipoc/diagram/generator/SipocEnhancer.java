bc450076e8bdca31fe2771c8f22303d7ac6433d7
Sese-collab <serpil.coskun@zenika.com>
2023-01-26 14:37:29 +0100

    generate sipoc table

--- sipoc-diagram-generator/src/main/java/org/ndx/aadarchi/sipoc/diagram/generator/SipocEnhancer.java
+++ sipoc-diagram-generator/src/main/java/org/ndx/aadarchi/sipoc/diagram/generator/SipocEnhancer.java
@@ -5,16 +5,10 @@ import org.ndx.aadarchi.base.AgileArchitectureSection;
 import org.ndx.aadarchi.base.OutputBuilder;
 import org.ndx.aadarchi.base.enhancers.ModelElementAdapter;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Default;
-import javax.enterprise.inject.Instance;
-import javax.inject.Inject;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-@ApplicationScoped
-@Default
+
 public class SipocEnhancer extends ModelElementAdapter {
 
     @Override
@@ -24,8 +18,24 @@ public class SipocEnhancer extends ModelElementAdapter {
 
     @Override
     public int priority() {
-        return 1;
+        return TOP_PRIORITY_FOR_INTERNAL_ENHANCERS;
     }
+
+    @Override
+    public boolean startVisit(SoftwareSystem softwareSystem) {
+        return true;
+    }
+
+    @Override
+    public boolean startVisit(Container container) {
+        return true;
+    }
+
+    @Override
+    public boolean startVisit(Component component) {
+        return true;
+    }
+
 
     @Override
     public void endVisit(Component c, OutputBuilder builder) {
@@ -41,16 +51,36 @@ public class SipocEnhancer extends ModelElementAdapter {
 
     @Override
     public void endVisit(SoftwareSystem softwareSystem, OutputBuilder builder) {
-        String sipocDiagram = sipocDiagramGenerator(c);
+        String sipocDiagram = sipocDiagramGenerator(softwareSystem);
         builder.writeToOutput(AgileArchitectureSection.code, softwareSystem,this, OutputBuilder.Format.adoc, sipocDiagram);
     }
 
+    /**
+     * Incoming relationship : afferent relationship that are connected
+     * Input : description of the incoming relationship
+     * Process : description of the current element
+     * Output : description off the outgoing relationship
+     * Outgoing relationship : efferent relationship connected at the exit
+     */
     String sipocDiagramGenerator(Element element) {
-    	// In the whole model
-        return element.getModel().getRelationships().stream()
-        	// Get relationships where element is source or destination
-        	.filter(relationship -> relationship.getSource().equals(element) || relationship.getDestination().equals(element))
-        	.map => string
-        	.collect => table
+        List<String> outcomingRelationshipsDescription =  element.getEfferentRelationshipsWith(element).stream()
+                .filter(relationship -> relationship.getDestination().equals(element))
+                .map(relationship -> relationship.getSource().getDescription())
+                .collect(Collectors.toList());
+
+        return  element.getModel().getRelationships().stream()
+                .map(s -> String.format("|%s|%s|%s|%s|%s", getAfferentRelationshipsWith(element).keySet(), getAfferentRelationshipsWith(element).values(), element.getDescription(), outcomingRelationshipsDescription, (element.getEfferentRelationshipsWith(element))))
+                .collect(Collectors.joining("\n\n\n\n\n", "[cols=\"1,1,1,1,1\"]\n" + "|===\n|Incoming|Input|Process|Output|Outgoing\n\n\n\n\n", "\n|==="));
+    }
+
+    public HashMap<String, String> getAfferentRelationshipsWith(Element element) {
+        HashMap<String, String> map = new HashMap<>();
+        Set<Relationship> relationships = element.getModel().getRelationships();
+        for (Relationship relationship : relationships) {
+            if (relationship.getSource().equals(element)) {
+                map.put(relationship.getDestination().getName(), relationship.getDestination().getDescription());
+            }
+        }
+        return map;
     }
 }
