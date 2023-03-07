265f5afa09c38f69fe8db7c6d5a5d516ae56e13f
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2023-03-07 11:53:30 +0100

    I have only modified the test regarding input can you. Please also modify test regarding output?

--- sipoc-diagram-generator/src/test/java/org/ndx/aadarchi/sipoc/diagram/generator/SipocModelTest.java
+++ sipoc-diagram-generator/src/test/java/org/ndx/aadarchi/sipoc/diagram/generator/SipocModelTest.java
@@ -5,17 +5,27 @@ import java.util.Set;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.structurizr.model.Relationship;
+
 public class SipocModelTest extends AbstractSipocTest {
 
 	@Test public void can_create_input_list() {
-		Set<String> buildIncomingRelationship = Set.of(inputContainer.getName());
+		// given
 		SipocModel sipocModel = new SipocModel();
+		// when
+		// then
+		Set<String> buildIncomingRelationship = Set.of(
+				String.format("%s - %s", inputContainer.getName(), inputContainer.getDescription()));
 		Assertions.assertThat(sipocModel.buildIncomingRelationships(centerContainer)).isEqualTo(buildIncomingRelationship);
 	}
 
 	@Test public void can_create_description_input_list() {
-		Set<String> buildIncomingRelationshipDescription = Set.of(inputContainer.getDescription());
+		// given
 		SipocModel sipocModel = new SipocModel();
+		Relationship relationship = inputContainer.getRelationships().iterator().next();
+		// when
+		// then
+		Set<String> buildIncomingRelationshipDescription = Set.of(relationship.getDescription());
 		Assertions.assertThat(sipocModel.buildIncomingRelationshipDescriptions(centerContainer)).isEqualTo(buildIncomingRelationshipDescription);
 	}
 
