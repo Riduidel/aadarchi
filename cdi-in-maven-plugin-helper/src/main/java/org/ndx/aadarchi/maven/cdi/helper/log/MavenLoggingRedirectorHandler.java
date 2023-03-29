6f7d2b0bb0aabf54436d611cd0ba861aaf5fa38e
Helielzel <33100097+Helielzel@users.noreply.github.com>
2023-03-29 15:09:21 +0200

    added shortenSource method, not used yet because of testing issues

--- cdi-in-maven-plugin-helper/src/main/java/org/ndx/aadarchi/maven/cdi/helper/log/MavenLoggingRedirectorHandler.java
+++ cdi-in-maven-plugin-helper/src/main/java/org/ndx/aadarchi/maven/cdi/helper/log/MavenLoggingRedirectorHandler.java
@@ -14,29 +14,44 @@ public class MavenLoggingRedirectorHandler extends Handler {
         this.mavenLog = log;
     }
 
+    public static String shortenSource(String originalSource) {
+        String[] parts = originalSource.split("\\.");
+        StringBuilder newSource = new StringBuilder();
+        for (int i = 0; i < parts.length; i++) {
+            if (i > 0) {
+                newSource.append(".");
+            }
+            newSource.append(parts[i].charAt(0));
+        }
+        return newSource.toString();
+    }
+
     @Override
     public void publish(LogRecord record) {
+        //String source = shortenSource(record.getSourceClassName());
     	Level level = record.getLevel();
 		if(level==Level.OFF)
     		return;
         int levelValue = level.intValue();
 		if (levelValue <= Level.FINEST.intValue()) {
-            mavenLog.debug("(" + record.getSourceClassName() + ") " + record.getMessage(), record.getThrown());
+
+            mavenLog.debug("<" + record.getSourceClassName() + "> " + record.getMessage(), record.getThrown());
         } else if(levelValue <=Level.FINER.intValue()) {
-            mavenLog.debug("(" + record.getSourceClassName() + ") " + record.getMessage(), record.getThrown());
+            mavenLog.debug("<" + record.getSourceClassName() + "> " + record.getMessage(), record.getThrown());
         } else if(levelValue <=Level.FINE.intValue()) {
-            mavenLog.debug("(" + record.getSourceClassName() + ") " + record.getMessage(), record.getThrown());
+            mavenLog.debug("<" + record.getSourceClassName() + "> " + record.getMessage(), record.getThrown());
         } else if(levelValue <=Level.CONFIG.intValue()) {
-            mavenLog.info("(" + record.getSourceClassName() + ") " + record.getMessage(), record.getThrown());
+            mavenLog.info("<" + record.getSourceClassName() + "> " + record.getMessage(), record.getThrown());
         } else if(levelValue <=Level.INFO.intValue()) {
-            mavenLog.info("(" + record.getSourceClassName() + ") " + record.getMessage(), record.getThrown());
+            mavenLog.info("<" + record.getSourceClassName() + "> " + record.getMessage(), record.getThrown());
         } else if(levelValue <=Level.WARNING.intValue()) {
-            mavenLog.warn("(" + record.getSourceClassName() + ") " + record.getMessage(), record.getThrown());
+            mavenLog.warn("<" + record.getSourceClassName() + "> " + record.getMessage(), record.getThrown());
         } else { //SEVERE
-            mavenLog.error("(" + record.getSourceClassName() + ") " + record.getMessage(), record.getThrown());
+            mavenLog.error("<" + record.getSourceClassName() + "> " + record.getMessage(), record.getThrown());
         }
     }
-
+    // before : [�[1;34mINFO�[m] (org.ndx.aadarchi.base.ArchitectureEnhancer) Running enhancement org.ndx.aadarchi.base.enhancers.scm.SCMProjectCheckouter took 00:00:00.021
+    // after  : [�[1;34mINFO�[m] <o.n.a.base.ArchitectureEnhancer> Running enhancement org.ndx.aadarchi.base.enhancers.scm.SCMProjectCheckouter took 00:00:00.021
     @Override
     public void flush() {
     }
