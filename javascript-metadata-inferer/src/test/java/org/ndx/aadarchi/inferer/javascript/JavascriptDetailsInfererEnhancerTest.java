14480b062a41b190f0bb109e79aea6a22c12f0e7
Helielzel <33100097+Helielzel@users.noreply.github.com>
2023-04-12 16:12:25 +0200

    adding test for javascript project visits, yet to implement

---
+++ javascript-metadata-inferer/src/test/java/org/ndx/aadarchi/inferer/javascript/JavascriptDetailsInfererEnhancerTest.java
@@ -0,0 +1,41 @@
+package org.ndx.aadarchi.inferer.javascript;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class JavascriptDetailsInfererEnhancerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void startVisit() {
+    }
+
+    @Test
+    void endVisit() {
+    }
+
+    @Test
+    void testStartVisit() {
+    }
+
+    @Test
+    void testEndVisit() {
+    }
+
+    @Test
+    void testStartVisit1() {
+    }
+
+    @Test
+    void testEndVisit1() {
+    }
+}
