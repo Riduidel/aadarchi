5e9aa4b4e0ee4bf362d2fdc422d71e51caa432f8
Helielzel <33100097+Helielzel@users.noreply.github.com>
2023-04-18 16:44:42 +0200

    modified pom.xml -> jackson core version

--- javascript-metadata-inferer/src/test/java/org/ndx/aadarchi/inferer/javascript/JavascriptDetailsInfererEnhancerTest.java
+++ javascript-metadata-inferer/src/test/java/org/ndx/aadarchi/inferer/javascript/JavascriptDetailsInfererEnhancerTest.java
@@ -1,14 +1,23 @@
 package org.ndx.aadarchi.inferer.javascript;
 
+import com.structurizr.Workspace;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.ndx.aadarchi.base.enhancers.ModelElementKeys;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 class JavascriptDetailsInfererEnhancerTest {
 
+    //call processModelElement to find package.json
     @BeforeEach
     void setUp() {
+
     }
 
     @AfterEach
@@ -16,26 +25,32 @@ class JavascriptDetailsInfererEnhancerTest {
     }
 
     @Test
-    void startVisit() {
+    public void ensure_package_json_is_loaded_from_one_folder() {
+        // Given
+        Workspace w = new Workspace("test", "ensure we can read package.json");
+        var system = w.getModel().addSoftwareSystem("testSystem");
+        var container = system.addContainer("packageJsonTest");
+        container.addProperty(ModelElementKeys.ConfigProperties.BasePath.NAME, "src/test/qvgdc-app");
+                //TODO write path of qvgdc as a string
+        System.out.println(container);
+        // When
+        // TODO call the right enhancer
+        // Then
+        Assertions.assertThat(container.getTechnology()).isNotNull();
     }
 
-    @Test
-    void endVisit() {
-    }
 
     @Test
-    void testStartVisit() {
-    }
-
-    @Test
-    void testEndVisit() {
-    }
-
-    @Test
-    void testStartVisit1() {
-    }
-
-    @Test
-    void testEndVisit1() {
+    public void ensure_package_json_is_loaded_from_github() {
+        // Given
+        Workspace w = new Workspace("test", "ensure we can read package.json");
+        var system = w.getModel().addSoftwareSystem("testSystem");
+        var container = system.addContainer("packageJsonTest");
+        container.addProperty(ModelElementKeys.Scm.PROJECT, "https://github.com/Zenika/qvgdc-app");
+                // TODO get gqvdc scm url
+        // When
+        // TODO call the right enhancer
+        // Then
+        Assertions.assertThat(container.getTechnology()).isNotNull();
     }
 }
