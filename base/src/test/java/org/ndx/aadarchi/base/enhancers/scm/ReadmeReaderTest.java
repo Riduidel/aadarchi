a3b722b972827163f7fdb8d75b7db01402936d8d
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2024-02-28 14:25:06 +0100

    Looks like te file isn't generated (maybe because the content read from cache is empty)

--- base/src/test/java/org/ndx/aadarchi/base/enhancers/scm/ReadmeReaderTest.java
+++ base/src/test/java/org/ndx/aadarchi/base/enhancers/scm/ReadmeReaderTest.java
@@ -1,13 +1,20 @@
 package org.ndx.aadarchi.base.enhancers.scm;
 
+import java.io.File;
+
 import javax.inject.Inject;
 
-import org.assertj.core.util.Arrays;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.provider.local.LocalFileSystem;
+import org.assertj.core.api.Assertions;
 import org.jboss.weld.junit5.EnableWeld;
 import org.jboss.weld.junit5.WeldInitiator;
 import org.jboss.weld.junit5.WeldSetup;
 import org.junit.jupiter.api.Test;
+import org.ndx.aadarchi.base.AgileArchitectureSection;
 import org.ndx.aadarchi.base.ArchitectureEnhancer;
+import org.ndx.aadarchi.base.OutputBuilder;
+import org.ndx.aadarchi.base.OutputBuilder.Format;
 import org.ndx.aadarchi.base.enhancers.ModelElementKeys;
 
 import com.structurizr.Workspace;
@@ -24,11 +31,19 @@ class ReadmeReaderTest {
 	@Test
 	void bug_81_can_read_a_local_readme_file() {
     	// Given
+		String METHOD_NAME = "bug_81_can_read_a_local_readme_file"; 
     	var w = new Workspace(getClass().getName(), "a test workspace");
-    	var system = w.getModel().addSoftwareSystem("The system to add a readme to");
+    	var system = w.getModel().addSoftwareSystem(getClass().getName()+"#"+METHOD_NAME);
     	system.addProperty(ModelElementKeys.ConfigProperties.BasePath.NAME, ".");
 		// When
     	enhancer.enhance(w, tested);
-	}
-
+    	// Then
+    	FileObject readme = enhancer.getOutputBuilder().outputFor(AgileArchitectureSection.code, system, tested, Format.adoc);
+    	Assertions.assertThat(readme.getFileSystem()).isInstanceOf(LocalFileSystem.class);
+    	// yeah obviously we generated a local file, so let's convert it to a classical file
+    	File readmeFile = readme.getPath().toFile();
+    	Assertions.assertThat(readmeFile).exists()
+    		.isNotEmpty()
+    		.content().contains("aadarchi");
+		}
 }
