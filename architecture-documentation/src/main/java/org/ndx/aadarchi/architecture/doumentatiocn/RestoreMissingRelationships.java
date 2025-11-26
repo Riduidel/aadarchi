45d0d15cda3614b98e7ad44a783ff8f244a80f1a
riduidel <nicolas.delsaux@gmail.com>
2025-11-26 10:09:47 +0100

    Seems like it depends upon a deprecated feature that I'll have to r

--- architecture-documentation/src/main/java/org/ndx/aadarchi/architecture/doumentatiocn/RestoreMissingRelationships.java
+++ architecture-documentation/src/main/java/org/ndx/aadarchi/architecture/doumentatiocn/RestoreMissingRelationships.java
@@ -1,10 +1,8 @@
 package org.ndx.aadarchi.architecture.doumentatiocn;
 
-import org.ndx.aadarchi.base.Enhancer;
 import org.ndx.aadarchi.base.OutputBuilder;
 import org.ndx.aadarchi.base.enhancers.ModelElementAdapter;
 
-import com.structurizr.model.CodeElement;
 import com.structurizr.model.Component;
 import com.structurizr.model.Container;
 
@@ -26,22 +24,22 @@ public class RestoreMissingRelationships extends ModelElementAdapter {
 	@Override
 	public void endVisit(Container container, OutputBuilder builder) {
 		Component arcitectureEnhancer = container.getComponentWithName("ArchitectureEnhancer");
-		for (Component c : container.getComponents()) {
-			boolean enhancer = false;
-			for(CodeElement code : c.getCode()) {
-				if(!enhancer) {
-					try {
-						Class<?> clazz = Class.forName(code.getType());
-						enhancer = Enhancer.class.isAssignableFrom(clazz);
-					} catch (ClassNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
-			if(enhancer) {
-				arcitectureEnhancer.uses(c, "Enhances architecture");
-			}
-		}
+//		for (Component c : container.getComponents()) {
+//			boolean enhancer = false;
+//			for(CodeElement code : c.getCode()) {
+//				if(!enhancer) {
+//					try {
+//						Class<?> clazz = Class.forName(code.getType());
+//						enhancer = Enhancer.class.isAssignableFrom(clazz);
+//					} catch (ClassNotFoundException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				}
+//			}
+//			if(enhancer) {
+//				arcitectureEnhancer.uses(c, "Enhances architecture");
+//			}
+//		}
 	}
 }
